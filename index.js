17f2297a1cc6b2cf840ec5d103e84bea83f03da1
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-03-01 17:25:23 +0700

    remove test codes

--- index.js
+++ index.js
@@ -11,16 +11,6 @@ const io = require('socket.io')(server, {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-  socket.on('my message', (msg) => {
-    console.log(`my message: ${msg}`);
-    io.emit('my broadcast', msg);
-  });
-
-  socket.on('chat message', (msg) => {
-    console.log(`chat message: ${msg}`);
-    io.emit('chat message', msg);
-  });
-
   socket.on('disconnect', () => {
     console.log('the user disconnected');
   });
