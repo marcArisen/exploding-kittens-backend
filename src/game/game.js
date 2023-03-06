cf653456775a49518b735a64c7e1247a4e7a3b18
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-03-06 20:58:28 +0700

    basic game class

---
+++ src/game/game.js
@@ -0,0 +1,46 @@
+/* eslint-disable prettier/prettier */
+const Deck = require('./deck');
+const Player = require('./player');
+/**
+ * Represents a game of Exploding Kittens.
+ */
+class Game {
+  /**
+   * Create a new game with the specified players.
+   * @param {string[]} playerNames - The names of the players.
+   */
+  constructor(playerNames) {
+    this.players = playerNames.map((name) => new Player(name));
+    this.deck = new Deck();
+    this.discardPile = [];
+    this.turn = 0;
+    this.phase = ["begin", "action", "draw", "end"];
+    this.numberOfPlayers = this.players.length;
+    this.currentPlayerIndex = Math.floor(Math.random() * 4); // random number 0-3 at the beginning of the game
+    this.currentPlayer = this.players[this.currentPlayerIndex]; // current player
+    
+  }
+
+  /**
+   * Deal cards to each player.
+   */
+  dealCards() {
+    this.players.forEach((player) => {
+      for (let i = 0; i < 7; i++) {
+        player.addCardToHand(this.deck.draw())
+      }
+    });
+  }
+  /**
+   * Add exploding Kitten.
+   */
+  addExplodingKittenCard() {
+    this.deck.generateBombedCat();
+    this.deck.shuffle();
+  }
+
+
+
+}
+
+module.exports = Game;
