bb49a9dd46895f5fb824585ffd64927dbf94c8dc
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-03-28 01:17:22 +0700

    add more test, card test, and fix some test

--- __tests__/test_deck.ts
+++ __tests__/test_deck.ts
@@ -5,31 +5,29 @@ describe("Deck information", () => {
 
     test("total card in the deck",() =>{
         expect(deck.cards.length).toEqual(56)
-        }
-    )
+    });
 
     test("shuffle work?",() =>{
         let temp_deck:Deck = deck
         temp_deck.shuffle()
         expect(deck.cards.every((card) => temp_deck.cards.includes(card))).toEqual(true)
-    })
+    });
 
     test("俺のターン!! Draw!!",() =>{
         let deck_size = deck.cards.length
         deck.draw()
         expect(deck.cards.length).not.toEqual(deck_size)
-    })
+    });
 
     test("Peek card",() => {
         let deck_size = deck.cards.length
         deck.peek(3)
         expect(deck.cards.length).toEqual(deck_size)
-    })
+    });
 
     test("Borbadier Cat",() => {
         let deck_size = deck.cards.length
         deck.generateBombedCat()
         expect(deck.cards.length).not.toEqual(deck_size)
-    })
-
-})
+    });
+});
